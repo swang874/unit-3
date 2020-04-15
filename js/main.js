3d72d93486df4e2d24a74ce34b177ee398136288
wshujin <54903336+wshujin@users.noreply.github.com>
2020-04-15 06:35:38 -0600

    Lab2 - part1
    coordinated visualization

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@
 //wrap everything in a self-executing anonymous function to move to local scope
 (function(){
 //pseudo-global variables for data join
-var attrArray = ["hospop", "c325pop", "d325c", "c401pop", "d401c"]; //list of attributes
+var attrArray = ["Hospital Beds", "Mar 25th Incidence Rate", "Mar 25th Death Rate", "Apr 1st Incidence Rate", "Apr 1st Death Rate"]; //list of attributes
+var domain = {"Hospital Beds": 5.5, "Mar 25th Incidence Rate": 3.5, "Mar 25th Death Rate": 120, "Apr 1st Incidence Rate": 7, "Apr 1st Death Rate": 150}
+var title = {"Hospital Beds": "Number of Hospital Beds per 1000 People",
+             "Mar 25th Incidence Rate": "Number of COVID-19 Cases per 1000 people on Mar 25th",
+             "Mar 25th Death Rate": "Number of Deaths per 1000 Cases of COVID-19 on Mar 25th",
+             "Apr 1st Incidence Rate": "Number of COVID-19 Cases per 1000 people on Apr 1st",
+             "Apr 1st Death Rate": "Number of Deaths per 1000 Cases of COVID-19 on Apr 1st"}
 var expressed = attrArray[2]; //initial attribute
+
+//chart frame dimensions
+var chartWidth = window.innerWidth * 0.425,
+    chartHeight = 460;
+
+//create a scale to size bars proportionally to frame
+var yScale = d3.scaleLinear()
+    .range([0, chartHeight])
+    .domain([0, domain[expressed]]);
 
 //begin script when window loads
 window.onload = setMap();
@@ -86,6 +101,8 @@ function setMap(){
             .attr("height", 200);
         setGraticule(map2, path2, 2);
         setEnumerationUnits(spainRegions, map2, path2, colorScale);
+
+        createDropdown(csvData);
     };
 };
 
@@ -113,7 +130,7 @@ function joinData(spainRegions, csvData){
     //loop through csv to assign each set of csv attribute values to geojson region
     for (var i=0; i<csvData.length; i++){
         var csvRegion = csvData[i]; //the current region
-        var csvKey = csvRegion.name; //the CSV primary key
+        var csvKey = csvRegion.NAME_1; //the CSV primary key
 
         //loop through geojson regions to find correct region
         for (var a=0; a<spainRegions.length; a++){
@@ -182,30 +199,30 @@ function setEnumerationUnits(spainRegions, map, path, colorScale){
         .style("fill", function(d){
             var value = d.properties[expressed];
             if(value) {
-            	return colorScale(d.properties[expressed]);
+            	return colorScale(value);
             } else {
             	return "#ccc";
             }
+        })
+        .on("mouseover", function(d){
+            highlight(d.properties);
+        })
+        .on("mouseout", function(d){
+            dehighlight(d.properties);
         });
+
+    var desc = regions.append("desc")
+        .text('{"stroke": "black", "stroke-width": "0.5px"}');
 };
 
 //function to create coordinated bar chart
 function setChart(csvData, colorScale){
-    //chart frame dimensions
-    var chartWidth = window.innerWidth * 0.425,
-        chartHeight = 460;
-
     //create a second svg element to hold the bar chart
     var chart = d3.select("body")
         .append("svg")
         .attr("width", chartWidth)
         .attr("height", chartHeight)
         .attr("class", "chart");
-
-    //create a scale to size bars proportionally to frame
-    var yScale = d3.scaleLinear()
-        .range([0, chartHeight])
-        .domain([0, 120]);
 
     //set bars for each region
     var bars = chart.selectAll(".bars")
@@ -216,21 +233,20 @@ function setChart(csvData, colorScale){
             return a[expressed]-b[expressed]
         })
         .attr("class", function(d){
-            return "bars " + d.name;
+            return "bars " + d.NAME_1;
         })
-        .attr("width", chartWidth / csvData.length - 1)
-        .attr("x", function(d, i){
-            return i * (chartWidth / csvData.length);
-        })
-        .attr("height", function(d){
-            return yScale(parseFloat(d[expressed]));
-        })
-        .attr("y", function(d){
-            return chartHeight - yScale(parseFloat(d[expressed]));
-        })
-        .style("fill", function(d){
-            return colorScale(d[expressed]);
-        });
+        .on("mouseover", highlight)
+        .on("mouseout", dehighlight);
+
+    var desc = bars.append("desc")
+        .text('{"stroke": "none", "stroke-width": "0px"}');
+
+    //create a text element for the chart title
+    var chartTitle = chart.append("text")
+        .attr("x", 20)
+        .attr("y", 40)
+        .attr("class", "chartTitle")
+        .text("Number of Deaths per 1000 Cases of COVID-19 on Mar 25th");
 
     //annotate bars with attribute value text
     var numbers = chart.selectAll(".numbers")
@@ -241,25 +257,149 @@ function setChart(csvData, colorScale){
             return a[expressed]-b[expressed]
         })
         .attr("class", function(d){
-            return "numbers " + d.name;
-        })
-        .attr("text-anchor", "middle")
+            return "numbers " + d.NAME_1;
+        });
+
+    //set bar positions, heights, colors and annotations
+    updateChart(bars, csvData.length, colorScale, numbers);
+};
+
+//function to position, size, color, and annotate bars in chart
+function updateChart(bars, n, colorScale, numbers){
+    //position bars
+    bars.attr("width", chartWidth / n - 1)
         .attr("x", function(d, i){
-            var fraction = chartWidth / csvData.length;
+            return i * (chartWidth / n);
+        })
+        //size/resize bars
+        .attr("height", function(d){
+            return yScale(parseFloat(d[expressed]));
+        })
+        .attr("y", function(d){
+            return chartHeight - yScale(parseFloat(d[expressed]));
+        })
+        //color/recolor bars
+        .style("fill", function(d){
+            var value = d[expressed];
+            if(value) {
+                return colorScale(value);
+            } else {
+                return "#ccc";
+            }
+        });
+
+    //annotate bars
+    numbers.attr("text-anchor", "middle")
+        .attr("x", function(d, i){
+            var fraction = chartWidth / n;
             return i * fraction + (fraction - 1) / 2;
         })
         .attr("y", function(d){
             return chartHeight - yScale(parseFloat(d[expressed])) + 15;
         })
         .text(function(d){
-            return Math.round(d[expressed]);
+            return parseFloat(d[expressed]).toFixed(1);
+        });
+};
+
+//function to create a dropdown menu for attribute selection
+function createDropdown(csvData){
+    //add select element
+    var dropdown = d3.select("body")
+        .append("select")
+        .attr("class", "dropdown")
+        .on("change", function(){
+            changeAttribute(this.value, csvData)
         });
 
-    //create a text element for the chart title
-    var chartTitle = chart.append("text")
-        .attr("x", 20)
-        .attr("y", 40)
-        .attr("class", "chartTitle")
-        .text("Number of Deaths per 1000 Cases of COVID-19 on Mar 25th");
+    //add initial option
+    var titleOption = dropdown.append("option")
+        .attr("class", "titleOption")
+        .attr("disabled", "true")
+        .text("Select Attribute");
+
+    //add attribute name options
+    var attrOptions = dropdown.selectAll("attrOptions")
+        .data(attrArray)
+        .enter()
+        .append("option")
+        .attr("value", function(d){ return d })
+        .text(function(d){ return d });
+};
+
+//dropdown change listener handler
+function changeAttribute(attribute, csvData){
+    //change the expressed attribute
+    expressed = attribute;
+
+    //recreate the color scale
+    var colorScale = makeColorScale(csvData);
+
+    //recolor enumeration units
+    var regions = d3.selectAll(".regions")
+        .transition()
+        .duration(1000)
+        .style("fill", function(d){
+            var value = d.properties[expressed];
+            if(value) {
+            	return colorScale(value);
+            } else {
+            	return "#ccc";
+            }
+    });
+
+    //change the scale to size bars proportionally to frame
+    yScale = d3.scaleLinear()
+        .range([0, chartHeight])
+        .domain([0, domain[expressed]]);
+
+    //re-sort, resize, recolor, and annotate bars
+    var bars = d3.selectAll(".bars")
+        .sort(function(a, b){
+            return a[expressed] - b[expressed];
+        })
+        .transition() //add animation
+        .delay(function(d, i){
+            return i * 20
+        })
+        .duration(500);
+    var numbers = d3.selectAll(".numbers")
+        .sort(function(a, b){
+            return a[expressed]-b[expressed]
+        });
+    updateChart(bars, csvData.length, colorScale, numbers);
+
+    //change the text element for the chart title
+    var chartTitle = d3.select("text")
+        .text(title[expressed]);
+};
+
+//function to highlight enumeration units and bars
+function highlight(props){
+    //change stroke
+    var selected = d3.selectAll("." + props.NAME_1)
+        .style("stroke", "black")
+        .style("stroke-width", "2");
+};
+
+//function to reset the element style on mouseout
+function dehighlight(props){
+    var selected = d3.selectAll("." + props.NAME_1)
+        .style("stroke", function(){
+            return getStyle(this, "stroke")
+        })
+        .style("stroke-width", function(){
+            return getStyle(this, "stroke-width")
+        });
+
+    function getStyle(element, styleName){
+        var styleText = d3.select(element)
+            .select("desc")
+            .text();
+
+        var styleObject = JSON.parse(styleText);
+
+        return styleObject[styleName];
+    };
 };
 })();
